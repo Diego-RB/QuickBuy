1e3c42df67e677883cedf12438ef3304f0071dcb
Diego Barbosa <diego1279.dr@hotmail.com>
2020-09-08 11:06:01 -0300

    Criar Entidades

---
+++ QuickBuy.Dominio/Enumerados/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Common;
+
+namespace QuickBuy.Dominio.Enumerados
+{
+   public enum TipoFormaPagamentoEnum
+    {
+        NaoDefinido = 0,
+        Boleto = 1,
+        CataoCredito = 2,
+        Depodito = 3
+        
+    }
+}
