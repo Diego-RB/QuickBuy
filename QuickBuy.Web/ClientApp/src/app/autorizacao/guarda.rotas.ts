df7d63644b2e30ad41f1a201004a804d2e56a882
Diego Barbosa <diego1279.dr@hotmail.com>
2020-09-29 09:53:04 -0300

    Controlando o acesso com o GuardaRotas

--- QuickBuy.Web/ClientApp/src/app/autorizacao/guarda.rotas.ts
+++ QuickBuy.Web/ClientApp/src/app/autorizacao/guarda.rotas.ts
@@ -10,7 +10,12 @@ export class GuardaRotar implements CanActivate {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    this.router.navigate(['/login']);
+    var autenticado = localStorage.getItem("usuario-autenticado");
+    if (autenticado == "1") {
+      return true;
+    }
+    //rota para redirecionar o usuario para a página de login.Ccaso o login estiver correto redireciona para a página solicitada
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
     //se o usuário for autenticado
     return false;
   }
--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app/login",
@@ -8,17 +9,19 @@ import { Usuario } from "../../modelo/usuario";
 })
 
 export class loginComponent {
+  //Uma das maneiras de endereçar uma imagem no HTML
   public enderecoImagem = "../../../assets/img/quick-login.jpg"
 
   public usuario;
     usuarioAutenticado: boolean;
-  constructor() {
+  constructor(private router: Router) {
     this.usuario = new Usuario();
   }
-
+  //função verifica se os dados informados na página login estão corretos e retorna 1 se for verdadeiro
   login() {
     if (this.usuario.email == "diego1279.dr@gmail.com" && this.usuario.senha == "123456") {
-      this.usuarioAutenticado = true;
+      localStorage.setItem("usuario-autenticado", "1");
+      //this.router.navigate(['/']);
     } 
   }
 }
