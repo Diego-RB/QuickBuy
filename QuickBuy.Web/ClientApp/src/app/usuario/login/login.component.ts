f94bca1004bb08b896934581faee016d7b3231b4
Diego Barbosa <diego1279.dr@hotmail.com>
2020-09-29 11:38:11 -0300

    Redirecionamento correto na autenticação

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app/login",
@@ -8,20 +8,27 @@ import { Router } from "@angular/router";
   styleUrls: ["./login.component.css"]
 })
 
-export class loginComponent {
+export class loginComponent implements OnInit {
   //Uma das maneiras de endereçar uma imagem no HTML
   public enderecoImagem = "../../../assets/img/quick-login.jpg"
+  public returnUrl: string;
 
   public usuario;
-    usuarioAutenticado: boolean;
-  constructor(private router: Router) {
-    this.usuario = new Usuario();
+  usuarioAutenticado: boolean;
+  constructor(private router: Router, private activateRouter: ActivatedRoute) {
+
   }
+    //Está fazendo a leitura do returnUrl do queryParams em guarda.rotas para quando efetuae login permanecer na página Produto
+    ngOnInit(): void {
+      this.returnUrl = this.activateRouter.snapshot.queryParams['returnUrl'];
+      this.usuario = new Usuario();
+    }
   //função verifica se os dados informados na página login estão corretos e retorna 1 se for verdadeiro
   login() {
     if (this.usuario.email == "diego1279.dr@gmail.com" && this.usuario.senha == "123456") {
       sessionStorage.setItem("usuario-autenticado", "1");
-      this.router.navigate(['/']);
+      //Após logado ele redirecina para a página que estava anteriormente.(Produto)
+      this.router.navigate([this.returnUrl]);
     } 
   }
 }
